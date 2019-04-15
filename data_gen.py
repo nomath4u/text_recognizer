b0783cbdd2022f7fa6291e50ec89d1e11902322e
Chris Harper <nomath4u@gmail.com>
2019-04-14 17:21:51 -0700

    Loop through fonts too

--- data_gen.py
+++ data_gen.py
@@ -1,5 +1,6 @@
 import numpy as np
 import string
+import os
 from PIL import Image, ImageFont, ImageDraw
 
 dim_x = 32
@@ -19,15 +20,22 @@ def MakeImg(t, f, fn, s = (100, 100), o = (16, 8)):
     draw.text(o, t, (255, 255, 255), font = f)
     img.save(fn)
 
-counter = 0
+def get_fonts():
+    fonts = []
+    for root, dirs, files in os.walk("/usr/share/fonts/truetype"):
+        for file in files:
+            if file.endswith(".ttf"):
+                fonts.append(file)
+    return fonts
 
+fonts = get_fonts()
 chars = list(string.ascii_letters) + list(string.digits)
-for tchar in chars:
-  font_name = "LiberationMono-Regular"
-  font = ImageFont.truetype(font_name + ".ttf", 16) #eventual loop through all fonts
-  size = font.getsize(tchar)
-  offset = ((dim_x - size[0]) // 2, (dim_y - size[1]) // 2)
-  rez = (dim_x,dim_y)
-  f_association = []
-  fname = resource_folder + font_name + "_" + tchar +".png"
-  MakeImg(tchar,font, fname ,rez,offset)
+for font_name in fonts:
+  for tchar in chars:
+    font = ImageFont.truetype(font_name + ".ttf", 16) #eventual loop through all fonts
+    size = font.getsize(tchar)
+    offset = ((dim_x - size[0]) // 2, (dim_y - size[1]) // 2)
+    rez = (dim_x,dim_y)
+    f_association = []
+    fname = resource_folder + font_name + "_" + tchar +".png"
+    MakeImg(tchar,font, fname ,rez,offset)
